df23d5beb4539cc304d32afd82a4bd26360537b7
Thutmose <elpatricimo@gmail.com>
2014-03-26 22:11:46 -0400

    added templates for asphalt

---
+++ src/main/java/thut/concrete/common/blocks/fluids/BlockLiquidAsphalt.java
@@ -0,0 +1,14 @@
+package thut.concrete.common.blocks.fluids;
+
+import net.minecraft.block.material.Material;
+import net.minecraftforge.fluids.Fluid;
+import thut.world.common.blocks.fluids.BlockFluid;
+
+public class BlockLiquidAsphalt extends BlockFluid {
+
+	public BlockLiquidAsphalt(Fluid fluid, Material par2) {
+		super(fluid, par2);
+		// TODO Auto-generated constructor stub
+	}
+
+}
