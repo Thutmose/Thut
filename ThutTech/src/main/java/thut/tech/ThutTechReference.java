900875b05de5d40fb2f72b6c378e395a6a64a5f1
Thutmose <elpatricimo@gmail.com>
2016-04-03 05:05:15 -0400

    comment out dependancy for development reasons

--- ThutTech/src/main/java/thut/tech/ThutTechReference.java
+++ ThutTech/src/main/java/thut/tech/ThutTechReference.java
@@ -7,7 +7,7 @@ public class ThutTechReference
     public static final String MOD_NAME           = "Thut's Tech";
     public static final String VERSION            = "@VERSION@";
     public final static String MCVERSIONS         = "[1.8.9]";
-    public final static String DEPSTRING          = "required-after:thutcore@[2.3.0,)";
+    public final static String DEPSTRING          = "";//required-after:thutcore@[2.3.0,)
     public static final String CLIENT_PROXY_CLASS = "thut.tech.client.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "thut.tech.common.CommonProxy";
 }
