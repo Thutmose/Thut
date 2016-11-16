050103bf41e76a64810aa8785b05c3a917431cfb
Thutmose <elpatricimo@gmail.com>
2016-11-16 18:55:07 -0400

    cleanup

--- ThutCore/src/main/java/thut/api/WorldCache.java
+++ ThutCore/src/main/java/thut/api/WorldCache.java
@@ -11,7 +11,6 @@ import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldType;
