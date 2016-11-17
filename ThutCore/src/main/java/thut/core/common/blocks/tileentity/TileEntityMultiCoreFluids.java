b7dada9449a3f1e958e68089db036343a2040480
Thutmose <elpatricimo@gmail.com>
2016-11-16 23:44:57 -0400

    remove the multiblock stuff, can remake better later if i need it.

--- ThutCore/src/main/java/thut/core/common/blocks/tileentity/TileEntityMultiCoreFluids.java
+++
@@ -1,8 +0,0 @@
-package thut.core.common.blocks.tileentity;
-
-import net.minecraftforge.fluids.IFluidHandler;
-
-public abstract class TileEntityMultiCoreFluids extends TileEntityMultiCore  implements IFluidHandler
-{
-
-}
