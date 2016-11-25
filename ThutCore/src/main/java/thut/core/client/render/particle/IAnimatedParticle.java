f29e7507565a787c3aea232041592d3641c8974c
Thutmose <elpatricimo@gmail.com>
2016-11-25 16:49:05 -0400

    move particle system here.

---
+++ ThutCore/src/main/java/thut/core/client/render/particle/IAnimatedParticle.java
@@ -0,0 +1,8 @@
+package thut.core.client.render.particle;
+
+public interface IAnimatedParticle
+{
+    void setAnimSpeed(int speed);
+    void setStartTime(int start);
+    void setTex(int[][] textures);
+}
