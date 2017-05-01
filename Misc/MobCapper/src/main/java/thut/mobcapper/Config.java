4777c29aec93b047b5158da7c43c7e803b6ab2a3
Thutmose <elpatricimo@gmail.com>
2017-05-01 17:11:30 -0400

    add logging to modcapper.

--- Misc/MobCapper/src/main/java/thut/mobcapper/Config.java
+++ Misc/MobCapper/src/main/java/thut/mobcapper/Config.java
@@ -6,6 +6,9 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class Config
 {
     public static boolean debug = false;
+    public static boolean logspawns = false;
+    public static boolean logCulls = false;
+    public static boolean client = false;
     public static int     number;
     public static int     number2;
 
@@ -23,6 +26,9 @@ public class Config
         number = config.getInt("maxnumber", Configuration.CATEGORY_GENERAL, 8, 1, 128,
                 "the maximum number of entities allowed in a cube of radius range");
         debug = config.getBoolean("debug", Configuration.CATEGORY_GENERAL, false, "debug prints");
+        logspawns = config.getBoolean("logspawns", Configuration.CATEGORY_GENERAL, false, "log spawns (WARNING LOG SPAM CAUSES LAG)");
+        logCulls = config.getBoolean("logculls", Configuration.CATEGORY_GENERAL, false, "log culls (WARNING LOG SPAM CAUSES LAG)");
+        client = config.getBoolean("client", Configuration.CATEGORY_GENERAL, false, "works on clients (this should be false if in a server's modpack)");
 
         config.save();
     }
