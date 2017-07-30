93bc4b40b3fdf18f6ddac2f78867c1452a5fc344
Thutmose <elpatricimo@gmail.com>
2017-07-30 04:01:26 -0400

    remove the failed attempt at a SMD renderer. improves model API
    Adds a way to register custom IModels for the ModelFactory.
    Adds IModelRenderer sensitive methods for IModelCustom rendering.

--- ThutCore/src/main/java/thut/core/client/render/model/ModelFactory.java
+++ ThutCore/src/main/java/thut/core/client/render/model/ModelFactory.java
@@ -1,13 +1,14 @@
 package thut.core.client.render.model;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 import java.util.Set;
 
 import com.google.common.collect.Maps;
 
+import net.minecraft.util.ResourceLocation;
 import thut.core.client.render.animation.ModelHolder;
 import thut.core.client.render.collada.DAEModel;
-import thut.core.client.render.smd.SMDModel;
 import thut.core.client.render.x3d.X3dModel;
 
 public class ModelFactory
@@ -17,15 +18,32 @@ public class ModelFactory
     static
     {
         validExtensions.put("x3d", X3dModel.class);
-        validExtensions.put("smd", SMDModel.class);
+        // validExtensions.put("smd", SMDModel.class);
         validExtensions.put("dae", DAEModel.class);
+    }
+
+    public static void registerIModel(String extension, Class<? extends IModel> clazz)
+    {
+        validExtensions.put(extension, clazz);
     }
 
     public static IModel create(ModelHolder model)
     {
-        if (model.model.getResourcePath().endsWith(".x3d")) return new X3dModel(model.model);
-        if (model.model.getResourcePath().endsWith(".smd")) return new SMDModel(model.model);
-        if (model.model.getResourcePath().endsWith(".dae")) return new DAEModel(model.model);
+        String path = model.model.getResourcePath();
+        String ext = path.substring(path.lastIndexOf(".") + 1, path.length());
+        Class<? extends IModel> clazz = validExtensions.get(ext);
+        if (clazz != null)
+        {
+            try
+            {
+                return clazz.getConstructor(ResourceLocation.class).newInstance(model.model);
+            }
+            catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                    | InvocationTargetException | NoSuchMethodException | SecurityException e)
+            {
+                e.printStackTrace();
+            }
+        }
         return null;
     }
 
