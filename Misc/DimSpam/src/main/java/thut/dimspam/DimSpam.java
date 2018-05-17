fa225ec33e60b970ca8d901f9cea832e94613d1a
Thutmose <elpatricimo@gmail.com>
2018-05-17 14:59:54 -0400

    remove an un-needed method

--- Misc/DimSpam/src/main/java/thut/dimspam/DimSpam.java
+++ Misc/DimSpam/src/main/java/thut/dimspam/DimSpam.java
@@ -18,8 +18,6 @@ import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 @Mod(modid = DimSpam.MODID, name = "Dimension Spam", version = DimSpam.VERSION, acceptableRemoteVersions = "*", acceptedMinecraftVersions = DimSpam.MCVERSIONS)
@@ -28,12 +26,6 @@ public class DimSpam
     public static final String MODID      = "dimspam";
     public static final String VERSION    = "1.0.0";
     public final static String MCVERSIONS = "[1.12.2]";
-
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
 
     @Mod.EventHandler
     public void serverStarting(FMLServerStartingEvent ev)
